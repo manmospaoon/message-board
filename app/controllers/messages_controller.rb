c66f19a659391c32b1c2570b11642e1a45030531
mark247 <takamamonosora.24@gmail.com>
2019-04-30 09:17:53 +0000

    DRY

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,11 @@
 class MessagesController < ApplicationController
+  before_action :set_message,only:[:show,:edit,:update,:destroy]
   def index
     @messages = Message.all
   end
   
   def show
-    @message = Message.find(params[:id])
+    set_message
   end
   
   def new
@@ -24,11 +25,11 @@ class MessagesController < ApplicationController
   end
   
   def edit
-    @message = Message.find(params[:id])
+    set_message
   end
   
   def update
-    @message = Message.find(params[:id])
+    set_message
     
     if @message.update(message_params)
       flash[:success] = 'Messageが正常に投稿されました。'
@@ -40,7 +41,7 @@ class MessagesController < ApplicationController
   end
   
   def destroy
-    @message = Message.find(params[:id])
+    set_message
     @message.destroy
     
     flash[:success] = 'Messageは正常に削除されました。'
@@ -48,6 +49,9 @@ class MessagesController < ApplicationController
   end
   
   private
+  def set_message
+    @message = Message.find(params[:id])
+  end  
   
   def message_params
     params.require(:message).permit(:content)
