438ed0f62c3c7be14a72829ad24fa1675c6a44cd
mark247 <takamamonosora.24@gmail.com>
2019-04-30 08:58:31 +0000

    CRUD messages

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,58 @@
+class MessagesController < ApplicationController
+  def index
+    @messages = Message.all
+  end
+  
+  def show
+    @message = Message.find(params[:id])
+  end
+  
+  def new
+    @message = Message.new
+  end
+  
+  def create
+    @message =  Message.new(message_params)
+    
+    if @message.save
+      flash[:success] = 'Messageが正常に投稿されました。'
+      redirect_to @message
+    else
+      flash.now[:denger] = 'Messageが投稿されませんでした。'
+      render :new
+    end
+  end
+  
+  def edit
+    @message = Message.find(params[:id])
+  end
+  
+  def update
+    @message = Message.find(params[:id])
+    
+    if @message.update(message_params)
+      flash[:success] = 'Messageが正常に投稿されました。'
+      redirect_to @message
+    else
+      flash.now[:denger] = 'Messageが投稿されませんでした。'
+      render :edit
+    end
+  end
+  
+  def destroy
+    @message = Message.find(params[:id])
+    @message.destroy
+    
+    flash[:success] = 'Messageは正常に削除されました。'
+    redirect_to messages_url
+  end
+  
+  private
+  
+  def message_params
+    params.require(:message).permit(:content)
+    #params.require(:message)でMessageモデルのフォームから得られるデータに関するものだと示す。
+    #permit(:content)でcontentカラムだけを選択。
+  end
+  
+end  
